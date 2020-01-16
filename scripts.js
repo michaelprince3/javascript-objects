af1bcaf3a03724a6eda653d0f3e95027fc3fa91f
Mike Prince <michaelprince3@gmail.com>
2020-01-16 15:31:26 -0600

    initial commit

---
+++ scripts.js
@@ -0,0 +1,97 @@
+const mustang = {
+    color: "red",
+    year: "2015",
+    make: "Ford"
+}
+
+const animals = [
+    "Kippers",
+    "Jack",
+    "Gypsy",
+    "Angus",
+    "Seymour Bouts",
+    "Sharky"
+]
+
+const wardrobe = {
+    height: 80,
+    manufacturer: "Killibrew & Sons",
+    contents: ["Dress shirt", "Jeans", "Suit", "Skirt", "Tennis shoes"],
+    depth: 38,
+    width: 50
+}
+
+console.log(wardrobe.height);
+console.log(wardrobe.manufacturer);
+console.log(wardrobe.contents);
+console.log(wardrobe.depth);
+console.log(wardrobe.width);
+
+const empireStateBuilding = {
+    stories: 103,
+    height: 1453,
+    address: "350 Fifth Avenue, Manhattan, New York 10118",
+    squareFeet: 2768591,
+    constructionDate: 1931,
+    cost: 40948900,
+    owner: "Empire State Realty Trust",
+    eastWestLength: 424,
+    northSouthLength: 187,
+    architect: "Shreve, Lamb & Harmon"
+};
+
+console.log(empireStateBuilding.stories);
+console.log(empireStateBuilding.height);
+console.log(empireStateBuilding.squareFeet);
+console.log(empireStateBuilding["address"]);
+console.log(empireStateBuilding["owner"]);
+
+const nashvilleSoftwareSchool = {
+    founded: 2012,
+    director: "John Wark",
+    instructors: {
+        fullTime: ["Jisie", "Brenda", "Steve", "Joe", "Andy"],
+        partTime: ["Zoe", "Nathan"]
+    },
+    address: "500 Interstate Blvd. S"
+}
+
+const fullTimeInst = nashvilleSoftwareSchool.instructors.fullTime
+for (let i = 0; i < fullTimeInst.length; i++) {
+    console.log(fullTimeInst[i])
+}
+
+console.log(nashvilleSoftwareSchool.instructors.fullTime[4])
+console.log(nashvilleSoftwareSchool.instructors.partTime[0])
+
+const beatles = {
+    albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour", "Something New"],
+    history: {
+        formed: 1960,
+        disbanded: 1970
+    },
+    members: [
+        {
+            name: "George Harrison",
+            birth: 1943,
+            death: 2001
+        },
+        {
+            name: "Paul McCartney",
+            birth: 1942,
+            death: null
+        },
+        {
+            name: "John Lennon",
+            birth: 1940,
+            death: 1980
+        },
+        {
+            name: "Ringo Starr",
+            birth: 1940,
+            death: null
+        }
+    ]
+}
+
+console.log(`${beatles.members[1].name} was in the Beatles from ${beatles.history.formed} to ${beatles.history.disbanded}. He was born in ${beatles.members[1].birth}. He contributed heavily to the ${beatles.albums[3]}.`)
